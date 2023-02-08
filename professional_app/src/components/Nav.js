145868431e3c56973343d08ea0a13ca8319c4657
Matt <mgstankov30@gmail.com>
2023-02-08 12:17:55 -0500

    add divs for the nav bar display styling

--- professional_app/src/components/Nav.js
+++ professional_app/src/components/Nav.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import '../styles/Nav.scss';
+
 const Nav = () => {
   return (
-    <div>
-      <Link to="/">
-        <img src="/images/Logo.pn" alt="Home" />
-      </Link>
-      <Link to="/About">About</Link>
-      <Link to="/Project">Projects</Link>
-      <Link to="/Experience">Experience</Link>
-      <Link to="/Contact">Contact</Link>
-      <Link
-        to={'https://www.cakeresume.com/s--8zZSLURFdyuhOyYRUyAZBg--/m-stanks'}
-        target="_blank"
-      >
-        <button>Resume</button>
-      </Link>
+    <div className="nav">
+      <div className="go-home">
+        <Link to="/">
+          <img src="/images/Logo.pn" alt="Home" />
+        </Link>
+      </div>
+      <div className="links">
+        <Link to="/About">About</Link>
+        <Link to="/Project">Projects</Link>
+        <Link to="/Experience">Experience</Link>
+        <Link to="/Contact">Contact</Link>
+        <Link
+          to={'https://www.cakeresume.com/s--8zZSLURFdyuhOyYRUyAZBg--/m-stanks'}
+          target="_blank"
+        >
+          <button>Resume</button>
+        </Link>
+      </div>
     </div>
   );
 };
