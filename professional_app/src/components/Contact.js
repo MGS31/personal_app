9a71958f4e3c8f646b8869c5e7857b14a3766fc9
Matt <mgstankov30@gmail.com>
2023-02-08 11:33:06 -0500

    add initial information for contact page

--- professional_app/src/components/Contact.js
+++ professional_app/src/components/Contact.js
@@ -4,6 +4,12 @@ const Contact = () => {
   return (
     <div>
       <p>Contact me here!</p>
+      <ul>
+        <li>Email: mgstankov30@gmail.com</li>
+        <li>Phone: 289-828-4077</li>
+        <li>github: https://github.com/MGS31</li>
+        <li>linkedin: https://www.linkedin.com/in/matt-s-6b92732b/</li>
+      </ul>
     </div>
   );
 };
