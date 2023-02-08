cda1f8a3136d109afdf4dfee6624d21c4150046e
Matt <mgstankov30@gmail.com>
2023-02-08 11:28:31 -0500

    add quick blurb for About page

--- professional_app/src/components/About.js
+++ professional_app/src/components/About.js
@@ -4,6 +4,17 @@ const About = () => {
   return (
     <div>
       <p>Learn about me here!</p>
+      <p>
+        After spending over a decade in media sales, first starting in
+        Television ad ops I found an interest in development when I moved to
+        digital advertisments. From here the interest further expanded during my
+        time on the data team at theWeatherNetwork. Seeing the work and
+        environment the developers spent their days in drove me to make the
+        leap. I enrolled in the Lighthouse Labs bootcamp in late 2021 and
+        completed the full course in August 2022. From then on I've been working
+        to improve and expand my learnings as a developer with different
+        languages and experiences.
+      </p>
     </div>
   );
 };
