5e8b05edb9e537b243baf05121e690f771ce6c9a
Matt <mgstankov30@gmail.com>
2023-02-08 11:23:31 -0500

    add home button as alt instead of logo for functionality

--- professional_app/src/components/Nav.js
+++ professional_app/src/components/Nav.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
   return (
     <div>
-      <Link to="/Home">
-        <img src="/images/Logo.png" alt="Matt's Logo" />
+      <Link to="/">
+        <img src="/images/Logo.pn" alt="Home" />
       </Link>
       <Link to="/About">About</Link>
       <Link to="/Project">Projects</Link>
