7ee0063aafda0e9cc9e1fce42e2291f544609bac
Matt <mgstankov30@gmail.com>
2023-02-08 12:17:35 -0500

    add div for home styling

--- professional_app/src/components/Home.js
+++ professional_app/src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import '../styles/Home.scss';
+
 const Home = () => {
   return (
     <>
-      <div>
+      <div className="home">
         <p>Hello World! My Name is Matt Stankovich</p>
         <Link to={'https://github.com/MGS31'} target="_blank">
           <button>Check out my GitHub</button>
